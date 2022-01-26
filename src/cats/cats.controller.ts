cf3d48621ac648761231c988bb6eadd335bb7356
erklyn <uenesgursel@gmail.com>
2022-01-26 13:48:07 +0300

    controllers

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Param, Post, Res } from '@nestjs/common';
-import { response } from 'express';
+import { Controller, Get, Param, Post, Body, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
-    return 'mock';
+  async create(@Body() createCatDto: CreateCatDto) {
+    return 'This action adds a new cat';
   }
   @Get()
-  findAll(): string {
-    return 'There is no cats sorry :(';
+  findAll(@Res() res: Response) {
+    res.status(200).json(['Cat1', 'Cat2']).end();
   }
   @Get(':id')
   findOne(@Param('id') id: string): string {
