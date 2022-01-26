01367fa7ce07d37084ef4e40d0989f13954969f4
erklyn <uenesgursel@gmail.com>
2022-01-26 13:38:39 +0300

    multiple endpoints dynamic routes

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Post, Res } from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('cats')
 export class CatsController {
+  @Post()
+  create(): string {
+    return 'mock';
+  }
   @Get()
   findAll(): string {
-    return 'I have found all the cats';
+    return 'There is no cats sorry :(';
+  }
+  @Get(':id')
+  findOne(@Param('id') id: string): string {
+    console.log(id);
+    return `Return the info of the ${id}. Cat`;
   }
 }
