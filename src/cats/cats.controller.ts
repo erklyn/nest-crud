9da9e381b32735e9c6ef656c7f9e1d22484e37fd
erklyn <uenesgursel@gmail.com>
2022-01-28 09:26:16 +0300

    Exception filters

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  Post,
+  Body,
+  UseFilters,
+  ForbiddenException,
+} from '@nestjs/common';
+import { HttpExceptionFilter } from '../common/filters/http-exception.filter';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dtos/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
@@ -7,8 +16,10 @@ import { Cat } from './interfaces/cat.interface';
 export class CatsController {
   constructor(private catsService: CatsService) {}
   @Post()
+  @UseFilters(HttpExceptionFilter)
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
+    throw new ForbiddenException();
   }
   @Get()
   async findAll(): Promise<Cat[]> {
