132de2d9d74a446f35aa2daba9e764209d67eab0
erklyn <uenesgursel@gmail.com>
2022-01-28 12:22:30 +0300

    formatted

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,13 +1,4 @@
-import {
-  Controller,
-  Get,
-  Param,
-  Post,
-  Body,
-  UseFilters,
-  ForbiddenException,
-} from '@nestjs/common';
-import { HttpExceptionFilter } from '../common/filters/http-exception.filter';
+import { Controller, Get, Param, Post, Body } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CreateCatDto } from './dtos/create-cat.dto';
 import { Cat } from './interfaces/cat.interface';
@@ -16,10 +7,8 @@ import { Cat } from './interfaces/cat.interface';
 export class CatsController {
   constructor(private catsService: CatsService) {}
   @Post()
-  @UseFilters(HttpExceptionFilter)
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
-    throw new ForbiddenException();
   }
   @Get()
   async findAll(): Promise<Cat[]> {
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './common/filters/all-exception.filter';
-import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
