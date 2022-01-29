51b059183c66afa395f60a2de6bc3f561e27629b
erklyn <uenesgursel@gmail.com>
2022-01-29 23:42:42 +0300

    connect with mysql

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,11 +1,15 @@
-import { SqliteConnectionOptions } from 'typeorm/driver/sqlite/SqliteConnectionOptions';
+import { MysqlConnectionOptions } from 'typeorm/driver/mysql/MysqlConnectionOptions';
 
-const config: SqliteConnectionOptions = {
-  type: 'sqlite',
-  database: 'tutorial',
-  entities: ['dist/src/**/*.entitiy.js '],
+const config: MysqlConnectionOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'nest-test',
+  entities: ['dist/src/**/*.entity.js '],
   //Auto drops and reinitiate a table so no no for prod
-  synchronize: false,
+  synchronize: true,
   migrations: ['dist/src/db/migrations/*.js'],
   cli: {
     migrationsDir: 'src/db/migrations',
