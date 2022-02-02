eb24ba2d490e9c278b1081029c366bcc58581951
erklyn <uenesgursel@gmail.com>
2022-02-02 20:14:32 +0300

    forgot to save

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,8 +7,6 @@ import { CatsController } from './cats/cats.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OwnerModule } from './owner/owner.module';
 import config from '../ormconfig';
-import { GraphQLModule } from '@nestjs/graphql';
-import { join } from 'path';
 
 @Module({
   imports: [CatsModule, TypeOrmModule.forRoot(config), OwnerModule],
